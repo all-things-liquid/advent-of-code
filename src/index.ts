8aad293dde89e020196abba074c05d235881c907
Sonia Seddiki <sseddiki@pitchy.fr>
2022-01-05 23:20:01 +0100

    Remove check after end of advent :)

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import logger from './logger'
 
 function main() {
   try {
-    const date = new Date()
-    const today = date.getDate()
-
-    for (let day = 1; day <= Math.min(today, 25); day++) {
+    for (let day = 1; day <= 25; day++) {
       if (solved(day)) {
         const { part1, part2 } = solutions[day]()
         logger.info({ part1, part2 }, `Solution of day ${day}`)
